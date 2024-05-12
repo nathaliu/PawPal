f0566b90c17e7aaa01b4e14634aa94da398ad1df
Nathalie Liu <liunathalie@hotmail.fr>
2024-05-12 23:28:19 +0200

    Correction - Code fix - Add new icons - App launchable now

--- app/src/main/java/com/example/pawpal/MainActivity.java
+++ app/src/main/java/com/example/pawpal/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.pawpal;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
 import com.example.pawpal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,27 +23,17 @@ public class MainActivity extends AppCompatActivity {
         binding.bottomNavigationView.setBackground(null);
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
-            switch (item.getItemId()) {
-                case R.id.nav_home:
-                    replaceFragment(new HomeFragment());
-                    break;
-
-                case R.id.nav_map:
-                    replaceFragment(new MapFragment());
-                    break;
-
-                case R.id.nav_chat:
-                    replaceFragment(new ChatFragment());
-                    break;
-
-                case R.id.nav_profile:
-                    replaceFragment(new ProfileFragment());
-                    break;
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.nav_map) {
+                replaceFragment(new MapFragment());
+            } else if (itemId == R.id.nav_chat) {
+                replaceFragment(new ChatFragment());
+            } else if (itemId == R.id.nav_profile) {
+                replaceFragment(new ProfileFragment());
             }
-
             return true;
-
         });
 
     }
